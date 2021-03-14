80846476073d4c8111503918e8522ee7137e96e0
qiujie <1160819635@qq.com>
2021-03-14 19:09:48 +0800

    tijao

---
+++ src/main/java/com/qiujie/jzb/JzbApplication.java
@@ -0,0 +1,13 @@
+package com.qiujie.jzb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JzbApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JzbApplication.class, args);
+    }
+
+}
