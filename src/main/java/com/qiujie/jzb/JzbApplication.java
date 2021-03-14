50ca0be119caac02c1ad400e3a6ada772ede82cc
qiujie <1160819635@qq.com>
2021-03-14 19:25:26 +0800

    提交

--- src/main/java/com/qiujie/jzb/JzbApplication.java
+++ src/main/java/com/qiujie/jzb/JzbApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JzbApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(JzbApplication.class, args);
     }
